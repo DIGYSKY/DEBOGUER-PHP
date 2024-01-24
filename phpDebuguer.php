47e3a2416c991f3b1dcf9a4b297a4a6deac7398e
DIGYSKY <lilyan.chauveau@gmail.com>
2024-01-24 14:47:24 +0100

    update to 2.1.1

--- phpDebuguer.php
+++ phpDebuguer.php
@@ -1,13 +1,15 @@
 <?php if (DEBUG) { ?>
     <div style="width:100%; border: 5px solid red; padding: 1em; background-color: white;">
-        <H1 style="text-align: center; margin-bottom: 1em;">DEBOGUER PHP 2.0.0</H1>
+        <H1 style="text-align: center; margin-bottom: 1em;">DEBUGUER PHP 2.1.1</H1>
         <p style="color: blue;">SUPERGLOBAL</p>
         <p style="color: red;">Object</p>
         <P style="margin-bottom: 2em;">Variable</P>
         <?php
         // Obtenir toutes les variables en mémoire
+        $pile = debug_backtrace();
         $variables = get_defined_vars();
         $display = [
+            'pile' => ['Pile d execution' => $pile],
             'session' => [],
             'other' => [],
             'object' => [],
@@ -23,20 +25,37 @@
                 $display['superglobal'][$key] = $value;
             } elseif (is_object($value)) {
                 $display['object'][$key] = $value;
-            } elseif ($key !== '_SESSION') {
+            } elseif ($key !== '_SESSION' && $key !== 'pile') {
                 $display['other'][$key] = $value;
             }
         }
 
+        $h2 = '';
+        $color = '';
+        $h2c = '';
+
         // Parcourir les variables
-        foreach ($display as $var) {
-            foreach ($var as $key => $value) { ?>
+        foreach ($display as $var) { 
+            foreach ($var as $key => $value) {
+                $letters = substr($key, 0, 1);
+                if ($letters === '_') {
+                    $h2 = 'Superglobales :';
+                    $color = 'blue';
+                } elseif (is_object($value)) {
+                    $h2 = 'Objects :';
+                    $color = 'red';
+                } elseif ($key !== '_SESSION' && $key !== 'Pile d execution') {
+                    $h2 = 'Variables :';
+                    $color = '';
+                } 
+                if ($h2 !== $h2c) { ?>
+                <h2 style="color: <?= $color ?>; margin-bottom: 1em;"><?= $h2 ?></h2> <?php } $h2c = $h2;?>
                 <div style='margin-bottom: 2em; border: 3px solid red; width: 80%; padding: 1em; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); border-radius: 1em; position: relative;'>
                     <?php
-                    $color = ($key[0] === '_') ? 'blue' : (is_object($value) ? 'red' : '');
+                    $color = ($key[0] === '_') ? 'blue' : (is_object($value) ? 'red' : ($key === 'Pile d execution' ? 'green' : ''));
                     ?>
                     <button onclick='toggleContent("<?php echo $key; ?>-content")' style='position: absolute; top: 2px; right: 10px; border-radius: 1em; font-size: 2em; padding: 8px 20px; cursor: pointer;'>+</button>
-                    <h2 style='color: <?php echo $color; ?>'><?php echo "$key : " . (empty($variables[$key]) ? 'empty' : ''); ?></h2>
+                    <h2 style='color: <?php echo $color; ?>'><?php echo "$key : " . (empty($variables[$key]) && $key !== 'Pile d execution' ? 'empty' : ''); ?></h2>
                     <div id='<?php echo "$key-content"; ?>' style='display: none;'>
                         <pre style="overflow: auto;">
                             <?php print_r($value); ?>
